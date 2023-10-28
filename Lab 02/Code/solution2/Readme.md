291d643a88b4711edfc85eaad682b536551692c6
Lahiru Bandara <lahirub@ceyentra.com>
2023-10-28 17:24:53 +0530

    Submission

---
+++ Lab 02/Code/solution2/Readme.md
@@ -0,0 +1,7 @@
+# CS4532 - Concurrent Programming
+## Lab 02 - 190077A,190601D
+
+### Instructions on running the program
+01. Open the provided directory in vscode.
+02. In the terminal enter `javac solution2.java`
+03. Next run the compiled program by entering `java solution2`
--- Lab 02/Code/solution2/solution2.java
+++ Lab 02/Code/solution2/solution2.java
@@ -1,63 +1,3 @@
-/* 
- * The Senate Bus problem:
-This problem was originally based on the Senate bus at Wellesley College. Riders
-come to a bus stop and wait for a bus. When the bus arrives, all the waiting
-riders invoke boardBus, but anyone who arrives while the bus is boarding has
-to wait for the next bus. The capacity of the bus is 50 people; if there are more
-than 50 people waiting, some will have to wait for the next bus.
-When all the waiting riders have boarded, the bus can invoke depart. If the
-bus arrives when there are no riders, it should depart immediately.
-
-Puzzle: Write synchronization code that enforces all of these constraints.
-
- * 
- * Solution: 
-Grant Hutchins came up with this solution, which uses fewer variables than the
-previous one, and doesn’t involve passing around any mutexes. Here are the
-variables:
-Bus problem solution #2 (initialization)
-1 waiting = 0
-2 mutex = new Semaphore (1)
-3 bus = new Semaphore (0)
-4 boarded = new Semaphore (0)
-
-waiting is the number of riders in the boarding area, which is protected by
-mutex. bus signals when the bus has arrived; boarded signals that a rider has
-boarded.
-
-Here is the code for the bus.
-
-Bus problem solution (bus):
-1 mutex . wait ()
-2 n = min ( waiting , 50)
-3 for i in range (n ):
-4 bus . signal ()
-5 boarded . wait ()
-6
-7 waiting = max ( waiting -50 , 0)
-8 mutex . signal ()
-9
-10 depart ()
-
-The bus gets the mutex and holds it throughout the boarding process. The
-loop signals each rider in turn and waits for her to board. By controlling the
-number of signals, the bus prevents more than 50 riders from boarding.
-When all the riders have boarded, the bus updates waiting, which is an
-example of the "I'll do it for you" pattern.
-
-The code for the riders uses two simple patterns: a mutex and a rendezvous.
-
-Bus problem solution (riders):
-1 mutex . wait ()
-2 waiting += 1
-3 mutex . signal ()
-4
-5 bus . wait ()
-6 board ()
-7 boarded . signal ()
-
- */
-
 package Code.solution2;
 
 import java.util.concurrent.Semaphore;
@@ -136,7 +76,6 @@ public class solution2 {
              * 
              * By controlling the number of signals, the bus prevents more than 50 riders from boarding. When all the riders have boarded, the bus updates waiting, which is an example of the "I'll do it for you" pattern.
              */
-            //while (true) {
             try {
                 mutex.acquire();
                 int n = Math.min(waiting, 50);
@@ -152,7 +91,6 @@ public class solution2 {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            // }
         }
 
         private void arrive(int n) {
@@ -168,12 +106,6 @@ public class solution2 {
      * The Rider class has a Semaphore mutex, which is used to protect the waiting variable.
      */
     static class Rider implements Runnable {
-        // private final int id;
-
-        // public Rider(int id) {
-        //     this.id = id;
-        // }
-
         /*  The run method of the Rider class increments the waiting variable, signals the bus Semaphore, and waits for the boarded Semaphore.
          */
         @Override
